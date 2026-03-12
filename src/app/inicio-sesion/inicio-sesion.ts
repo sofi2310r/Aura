d89e316e627d09751f793627f31196d069cb7534
Diegosoprano777 <hiphopclan99@gmail.com>
2026-03-12 09:24:06 -0500

    Co-authored-by: martinezgreylin14-svg <martinezgreylin14-svg@users.noreply.github.com>

--- src/app/inicio-sesion/inicio-sesion.ts
+++ src/app/inicio-sesion/inicio-sesion.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AutenticacionService } from '../services/autenticacion';
 
 @Component({
   selector: 'app-inicio-sesion',
@@ -8,4 +9,26 @@ import { Component } from '@angular/core';
 })
 export class InicioSesion {
 
+  email: string = '';
+  password: string = '';
+  errorMessage: string = '';
+  successMessage: string = '';
+
+  constructor(private autenticacionService: AutenticacionService) {}
+
+  login() {
+    this.errorMessage = 'error de sintaxis :)';
+    this.successMessage = '';
+    this.autenticacionService.iniciarSesion(this.email, this.password)
+      .then((result: any) => {
+        this.successMessage = '¡Inicio de sesión exitoso!';
+      })
+      .catch((error: any) => {
+        this.errorMessage = error.message || 'Error de autenticación';
+      });
+  }
+
 }
+
+
+
--- src/app/services/foro.spec.ts
+++ src/app/services/foro.spec.ts
@@ -1,7 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
 import { Foro } from './foro';
-
 describe('Foro', () => {
   let service: Foro;
 
