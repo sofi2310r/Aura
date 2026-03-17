2b5e99c40e02e0567f2bcfdcbb388e36e30e4391
BrayanMR <brian@somosrocas.com>
2026-03-16 22:53:41 -0500

    Fix-f-routing-module-and-component-declarations (16-03-2026)

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -1,57 +1,42 @@
 import { NgModule, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { getAuth, provideAuth } from '@angular/fire/auth';
-import { getFirestore, provideFirestore } from '@angular/fire/firestore';
-import { environment } from '../environments/environment';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { InicioSesion } from './inicio-sesion/inicio-sesion';
-import { Registro } from './registro/registro';
-import { Navbar } from './components/navbar/navbar';
-import { Hero } from './components/hero/hero';
-import { Features } from './components/features/features';
-import { Empowerment } from './components/empowerment/empowerment';
-import { Home } from './pages/home/home';
-import { FooterBanner } from './components/footer-banner/footer-banner';
-import { Foro } from './pages/foro/foro';
-import { ListaPublicaciones } from './components/foro/lista-publicaciones/lista-publicaciones';
-import { NuevaPublicacion } from './components/foro/nueva-publicacion/nueva-publicacion';
-import { DetallePublicacion } from './components/foro/detalle-publicacion/detalle-publicacion';
-import { AvisoNormas } from './components/foro/aviso-normas/aviso-normas';
+import { LoginComponent } from '../components/auth/login.component';
+import { HomeComponent } from '../components/home/home.component';
+import { ForoComponent } from '../components/home/foro/foro.component';
+import { CitasComponent } from '../components/psicologo/citas/citas.component';
+import { PacientesComponent } from '../components/psicologo/pacientes/pacientes.component';
+import { ModeracionComponent } from '../components/moderador/moderacion/moderacion.component';
+import { NavbarComponent } from '../components/home/navbar/navbar.component';
+import { FooterComponent } from '../components/home/footer/footer.component';
 
 @NgModule({
   declarations: [
     App,
-    InicioSesion,
-    Registro,
-    Navbar,
-    Hero,
-    Features,
-    Empowerment,
-    Home,
-    FooterBanner,
-    Foro,
-    ListaPublicaciones,
-    NuevaPublicacion,
-    DetallePublicacion,
-    AvisoNormas
+    LoginComponent,
+    HomeComponent,
+    ForoComponent,
+    CitasComponent,
+    PacientesComponent,
+    ModeracionComponent,
+    NavbarComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule, 
+    HttpClientModule,
+    AppRoutingModule,
     CommonModule,
     FormsModule,
   ],
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideFirebaseApp(() => initializeApp({ projectId: "aura-d3e5f", appId: "1:436823356091:web:406b901367e080001c6f9e", storageBucket: "aura-d3e5f.firebasestorage.app", apiKey: "AIzaSyDpURBb4fPDEVJ5om7CK7LaHjDUSOL7dpE", authDomain: "aura-d3e5f.firebaseapp.com", messagingSenderId: "436823356091", measurementId: "G-GGSMJGF5PN" })),
-    provideAuth(() => getAuth()),
-    provideFirestore(() => getFirestore())
   ],
   bootstrap: [App]
 })
--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,11 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { Home } from './pages/home/home';
-import { Foro } from './pages/foro/foro';
+import { LoginComponent } from '../components/auth/login.component';
+import { HomeComponent } from '../components/home/home.component';
+import { ForoComponent } from '../components/home/foro/foro.component';
+import { AdminDashboardComponent } from '../components/admin/admin-dashboard/admin-dashboard.component';
+import { UsuariosComponent } from '../components/admin/usuarios/usuarios.component';
+import { CitasComponent } from '../components/psicologo/citas/citas.component';
+import { PacientesComponent } from '../components/psicologo/pacientes/pacientes.component';
+import { ModeracionComponent } from '../components/moderador/moderacion/moderacion.component';
 
 const routes: Routes = [
-  { path: '', component: Home },
-  { path: 'foro', component: Foro },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'foro', component: ForoComponent },
+  { path: 'admin', component: AdminDashboardComponent },
+  { path: 'admin/usuarios', component: UsuariosComponent },
+  { path: 'psicologo/citas', component: CitasComponent },
+  { path: 'psicologo/pacientes', component: PacientesComponent },
+  { path: 'moderador', component: ModeracionComponent },
+  { path: '**', redirectTo: 'login' },
 ];
 
 @NgModule({
